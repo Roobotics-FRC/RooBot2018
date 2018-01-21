f51d96b4886275c8bbd3f4156f5a5249da25fdae
Rui-Jie Fang <ruijie.fang@temple.edu>
2018-01-21 14:56:34 -0500

    Assert WPILib version in System assertions

--- src/test/java/org/usfirst/frc/team4373/robot/MainSystemTest.java
+++ src/test/java/org/usfirst/frc/team4373/robot/MainSystemTest.java
@@ -1,7 +1,29 @@
 package org.usfirst.frc.team4373.robot;
 
+import edu.wpi.first.wpilibj.util.WPILibVersion;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 /**
- * Created by derros on 1/21/18.
+ * Created by thefangbear on 1/21/18.
  */
 public class MainSystemTest {
+
+    @Before
+    public void setUp() throws Exception {
+        System.out.println("setUp(): Running MainSystemTest/runSystemAssertions...");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void runSystemAssertions() throws Exception {
+        // assert WPILib version
+        Assert.assertEquals(WPILibVersion.Version, "2018.1.1");
+    }
 }
